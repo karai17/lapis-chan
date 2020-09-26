a95e4175c9d400df2d78d0e79edd7083e866ef67
Landon Manning <lmanning17@gmail.com>
2020-09-26 20:38:39 -0300

    temporarily disabled the installer while i rethink how to go about this

--- app/app.lua
+++ app/app.lua
@@ -7,14 +7,14 @@ app.include = function(self, a)
 	self.__class.include(self, a, nil, self)
 end
 
--- Installer
-local ok, install = pcall(require, "install")
-if ok then
+--[[ -- app:before_filter(require "apps.web.global.install") -- FIXME: set up installer as a simple before filter
+do
 	local r2 = require("lapis.application").respond_to
 	app:before_filter(require "apps.web.global.config_site")
-	app:match("/", r2(install))
+	app:match("/", r2(require "apps.web.global.install"))
 	return app
 end
+--]]
 
 app:before_filter(require "apps.web.global.config_site")
 app:before_filter(require "apps.web.global.check_auth")
@@ -23,6 +23,5 @@ app:before_filter(require "apps.web.global.check_ban")
 app:include("apps.web.admin")
 app:include("apps.web.pages")
 app:include("apps.web.boards")
-
 
 return app
--- app/src/apps/web/boards.lua
+++ app/src/apps/web/boards.lua
@@ -4,11 +4,12 @@ local app   = lapis.Application()
 
 app.__base     = app
 app.name       = "web.boards."
+app.path       = "/board"
 app.handle_404 = require "apps.web.global.code_404"
 
-app:match("board",   "/board/:board(/page/:page)",                r2(require "apps.web.boards.board"))
-app:match("catalog", "/board/:board/catalog",                     r2(require "apps.web.boards.catalog"))
-app:match("archive", "/board/:board/archive",                     require "apps.web.boards.archive")
-app:match("thread",  "/board/:board/thread/:thread(#:anchor:id)", r2(require "apps.web.boards.thread"))
+app:match("board",   "/:board(/page/:page)",                r2(require "apps.web.boards.board"))
+app:match("catalog", "/:board/catalog",                     r2(require "apps.web.boards.catalog"))
+app:match("archive", "/:board/archive",                     require "apps.web.boards.archive")
+app:match("thread",  "/:board/thread/:thread(#:anchor:id)", r2(require "apps.web.boards.thread"))
 
 return app
